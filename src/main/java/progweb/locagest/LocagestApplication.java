372c87e2e0d809fd33a0096e8ba562eadba6c7d2
Igor Soares <igorsoares052@aluno.ueg.br>
2025-09-10 21:17:45 -0300

    Initial commit

---
+++ src/main/java/progweb/locagest/LocagestApplication.java
@@ -0,0 +1,13 @@
+package progweb.locagest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocagestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LocagestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/progweb/locagest/LocagestApplicationTests.java
@@ -0,0 +1,13 @@
+package progweb.locagest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LocagestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
